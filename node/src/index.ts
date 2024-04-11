ec36af6ac2a27e179f3dcae8cdc0b8bfef9f1a67
David Herron <david@davidherron.com>
2024-04-12 00:42:16 +0300

    Switch to Zod validators

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,17 +1,47 @@
 
 
-export { Project } from './types-evchargingspec/project.js';
-export { Station } from './types-evchargingspec/station.js';
-export { Port } from './types-evchargingspec/port.js';
-export { OperatingCosts } from './types-evchargingspec/operating-costs.js';
-export { Session } from './types-evchargingspec/session.js';
-export { Outage } from './types-evchargingspec/outage.js';
-export { Uptime } from './types-evchargingspec/uptime.js';
+export * from './types-evchargingspec/project.js';
+export * from './types-evchargingspec/station.js';
+export * from './types-evchargingspec/port.js';
+export * from './types-evchargingspec/operating-costs.js';
+export * from './types-evchargingspec/session.js';
+export * from './types-evchargingspec/outage.js';
+export * from './types-evchargingspec/uptime.js';
 
-export * from './validate/project.js';
-export * from './validate/station.js';
-export * from './validate/port.js';
-export * from './validate/operating-costs.js';
-export * from './validate/session.js';
-export * from './validate/outage.js';
-export * from './validate/uptime.js';
+// export * from './validate/project.js';
+// export * from './validate/station.js';
+// export * from './validate/port.js';
+// export * from './validate/operating-costs.js';
+// export * from './validate/session.js';
+// export * from './validate/outage.js';
+// export * from './validate/uptime.js';
+
+export {
+    parseProject, safeParseProject
+} from './validate-zod/project.js';
+
+export {
+    parseStation, safeParseStation
+} from './validate-zod/station.js';
+
+export {
+    parsePort, safeParsePort
+} from './validate-zod/port.js';
+
+export {
+    parseOperatingCosts,
+    safeParseOperatingCosts
+} from './validate-zod/operating-costs.js';
+
+export {
+    parseSession, safeParseSession
+} from './validate-zod/session.js';
+
+export {
+    parseOutage, safeParseOutage
+} from './validate-zod/outage.js';
+
+export {
+    parseUptime, safeParseUptime
+} from './validate-zod/uptime.js';
+
