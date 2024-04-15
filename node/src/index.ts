1eac8a51c3eb3c9e3d9c4961d68fba19eef714ee
David Herron <david@davidherron.com>
2024-04-15 13:25:16 +0300

    Remove -zod support

--- node/src/index.ts
+++ node/src/index.ts
@@ -8,40 +8,10 @@ export * from './types-evchargingspec/session.js';
 export * from './types-evchargingspec/outage.js';
 export * from './types-evchargingspec/uptime.js';
 
-// export * from './validate/project.js';
-// export * from './validate/station.js';
-// export * from './validate/port.js';
-// export * from './validate/operating-costs.js';
-// export * from './validate/session.js';
-// export * from './validate/outage.js';
-// export * from './validate/uptime.js';
-
-export {
-    parseProject, safeParseProject
-} from './validate-zod/project.js';
-
-export {
-    parseStation, safeParseStation
-} from './validate-zod/station.js';
-
-export {
-    parsePort, safeParsePort
-} from './validate-zod/port.js';
-
-export {
-    parseOperatingCosts,
-    safeParseOperatingCosts
-} from './validate-zod/operating-costs.js';
-
-export {
-    parseSession, safeParseSession
-} from './validate-zod/session.js';
-
-export {
-    parseOutage, safeParseOutage
-} from './validate-zod/outage.js';
-
-export {
-    parseUptime, safeParseUptime
-} from './validate-zod/uptime.js';
-
+export * from './validate/project.js';
+export * from './validate/station.js';
+export * from './validate/port.js';
+export * from './validate/operating-costs.js';
+export * from './validate/session.js';
+export * from './validate/outage.js';
+export * from './validate/uptime.js';
