d9baf4a6b8fda321657d2af36aacde57d34e8cf7
David Herron <david@davidherron.com>
2024-04-16 16:46:46 +0300

    Support YAML parsing and serialization, as well as arrays

--- node/src/validate/common.ts
+++ node/src/validate/common.ts
@@ -1,9 +1,11 @@
 
 import { promises as fsp } from 'node:fs';
 import path from 'node:path';
+import { Readable } from 'node:stream';
 import Ajv, { JSONSchemaType, DefinedError } from "ajv";
 import YAML from 'js-yaml';
-import { generate, parse, transform, stringify } from "csv/sync";
+import { generate, transform, stringify } from "csv/sync";
+import { parse } from 'csv-parse';
 
 import addFormats from "ajv-formats";
 export const ajv = new Ajv.default({
@@ -65,7 +67,11 @@ export type serializorOptions = {
  * @param options Serialization options
  * @returns
  */
-export function serializor<T>(data: T | Array<T>, validator: Ajv.ValidateFunction<T>, options: serializorOptions): errorResult {
+export function serializor<T>(
+        data: T | Array<T>,
+        validator: Ajv.ValidateFunction<T>,
+        options: serializorOptions
+): errorResult {
     let validated = false;
     if (Array.isArray(data)) {
         for (const datum of data) {
@@ -96,11 +102,15 @@ export function serializor<T>(data: T | Array<T>, validator: Ajv.ValidateFunctio
                 _options.record_delimiter = options.record_delimiter;
             }
             return {
-                result: stringify(data as any, _options)
+                result: stringify(
+                    Array.isArray(data)
+                    ? (data as any)
+                    : [ data as any ],
+                    _options)
             };
         } else if (options.format === 'JSON') {
             return {
-                result: JSON.stringify(data)
+                result: JSON.stringify(data, null, 4)
             };
         } else if (options.format === 'YAML') {
             return {
@@ -120,20 +130,53 @@ export function serializor<T>(data: T | Array<T>, validator: Ajv.ValidateFunctio
     }
 }
 
-export function validator<T>(data: T, validator: Ajv.ValidateFunction<T>): boolean {
-    if (validator(data)) {
+export function validator<T>(
+    data: T | Array<T>, validator: Ajv.ValidateFunction<T>
+): boolean {
+    if (Array.isArray(data)) {
+        for (const item of data) {
+            if (!validator(item)) {
+                // console.log(`validator array item ${YAML.dump({
+                //     item
+                // }, { indent: 4 })} failed`, validator.errors);
+                return false;
+            }
+        }
+        // console.log(`validation succeeds array ${YAML.dump({
+        //     data
+        // }, { indent: 4 })}`);
+        return true;
+    } else if (validator(data)) {
+        // console.log(`validation succeeds item ${YAML.dump({
+        //     data
+        // }, { indent: 4 })}`);
         return true;
     } else {
         // console.log(validator.errors);
+        // console.log(`validator item ${YAML.dump({
+        //     data
+        // }, { indent: 4 })} failed`, validator.errors);
         return false;
     }
 }
 
-export function parserJSON<T>(data: string, validator: Ajv.ValidateFunction<T>): T | undefined {
+export function parserJSON<T>(
+    data: string, validator: Ajv.ValidateFunction<T>
+): T | undefined {
     if (data && typeof data === 'string') {
         const ret: T = JSON.parse(data);
-        if (validator(ret)) {
+        if (Array.isArray(ret)) {
+            for (const item of ret) {
+                if (!validator(item)) {
+                    // console.log(`validator item ${YAML.dump({
+                    //     item
+                    // }, { indent: 4 })} fail`, validator.errors)
+                    return false;
+                }
+            }
             return ret;
+        } else if (validator(ret)) {
+            return ret as T;
         } else {
             // TODO how to indicate the errors
             return undefined;
@@ -141,6 +184,91 @@ export function parserJSON<T>(data: string, validator: Ajv.ValidateFunction<T>):
     } else {
         return undefined;
     }
+}
+
+export function parserYAML<T>(
+    data: string, validator: Ajv.ValidateFunction<T>
+): any {
+    if (data && typeof data === 'string') {
+        const ret: T = YAML.load(data);
+        // console.log(YAML.dump({
+        //     title: 'parserYAML',
+        //     data,
+        //     ret
+        // }, { indent: 4 }));
+        if (Array.isArray(ret)) {
+            for (const item of ret) {
+                if (!validator(item)) {
+                    console.log(`validator item ${YAML.dump({
+                        item
+                    }, { indent: 4 })} fail`, validator.errors)
+                    return false;
+                }
+            }
+            return ret;
+        } else if (validator(ret)) {
+            return ret as any;
+        } else {
+            // TODO how to indicate the errors
+            console.log('validator fail', validator.errors)
+            return undefined;
+        }
+    } else {
+        console.log('data not string', data);
+        return undefined;
+    }
+}
+
+export async function parseCSV<T>(
+    data: string | Readable,
+    processRecord: (record?: any[]) => T,
+    options?: any
+)
+    : Promise<Array<T> | undefined>
+{
+
+    // The csv-parse module works best with
+    // data arriving through a REadable.
+    // This block concocts a REadable
+    // from a string as noted here:
+    // https://stackoverflow.com/questions/12755997/how-to-create-streams-from-string-in-node-js
+    
+    let reader;
+    if (data instanceof Readable) {
+        reader = data;
+    } else if (typeof data === 'string') {
+        reader = Readable.from([ data ]);
+    } else {
+        throw new Error(`data parameter must be a string or a Readable`);
+    }
+
+    const records = [] as any[];
+    await new Promise((resolve, reject) => {
+
+        const parser = parse(options);
+      
+          parser.on('readable', function(){
+              let record;
+              while ((record = parser.read()) !== null) {
+                  records.push(processRecord(record));
+              }
+        });
+      
+        parser.on('error', function(err: any){
+            reject(err);
+        });
+    
+        parser.on('end', function() {
+            resolve(records);
+        });
+
+        reader.pipe(parser);
+        // parser.write(data);
+        // parser.end();
+
+    });
+
+    return records;
 }
 
 /**
